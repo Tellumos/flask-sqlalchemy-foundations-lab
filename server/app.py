082111dae48d5d4ee0b1f27ceae876341a77791b
mvrz <msv930@gmail.com>
2025-12-17 23:44:43 -0600

    earthquake data

--- server/app.py
+++ server/app.py
@@ -21,7 +21,41 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+    if earthquake:
+        response_body = {
+            'id': earthquake.id,
+            'location': earthquake.location,
+            'magnitude': earthquake.magnitude,
+            'year': earthquake.year
+        }
+        response_status = 200
+    else:
+        response_body = {
+            'message': f'Earthquake {id} not found.'
+        }
+        response_status = 404
+    response = make_response(response_body, response_status)
+    return response
 
-
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquakes_by_magnitude(magnitude):
+    earthquakes = []
+    for earthquake in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all():
+        earthquake_dict = {
+            'id': earthquake.id,
+            'location': earthquake.location,
+            'magnitude': earthquake.magnitude,
+            'year': earthquake.year
+        }
+        earthquakes.append(earthquake_dict)
+    response_body = {
+        'count': len(earthquakes),
+        'quakes': earthquakes
+    }
+    response = make_response(response_body, 200)
+    return response
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
